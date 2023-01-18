a14c9ee4e61ba5a62cde5171f37fdd3f2c9f7219
Mahesh Kumar <99642266+maheshkmrit@users.noreply.github.com>
2023-01-18 09:07:04 +0530

    Utilities for Input Output

---
+++ Utilities.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Utilities 
+{
+  internal static string Prompt(string question) 
+  {
+    Console.WriteLine(question);
+    return Console.ReadLine();
+  }
+
+  internal static int GetNumber(string question) 
+  {
+    bool processing = false;
+    int result;
+    do {
+      Console.WriteLine(question);
+      processing = int.TryParse(Console.ReadLine(), out result);
+    } while (!processing);
+    return result;
+  }
+
+}
